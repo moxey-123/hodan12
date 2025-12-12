368535a199323585d569c0ddc4ebf5987b094ef3
moxey-123 <ali.abdi.mohamud2024@gmail.com>
2025-12-12 01:02:05 -0800

    Add admin create-admin route

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -1,30 +1,3 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const Admin = require("../models/Admin");
-
-const router = express.Router();
-
-// Admin login
-router.post("/login", async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        const admin = await Admin.findOne({ username });
-        if (!admin) return res.status(400).json({ message: "Invalid username" });
-
-        const isMatch = await bcrypt.compare(password, admin.password);
-        if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
-
-        const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
-            expiresIn: "1d",
-        });
-
-        res.json({ message: "Login successful", token });
-    } catch (error) {
-        res.status(500).json({ message: "Server error" });
-    }
-});
 // TEMP: Create admin account (use once)
 router.post("/create-admin", async (req, res) => {
     const { username, password } = req.body;
@@ -34,6 +7,3 @@ router.post("/create-admin", async (req, res) => {
 
     res.send("Admin created");
 });
-
-
-module.exports = router;
